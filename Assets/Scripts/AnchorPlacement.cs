b249e4f8b20b7a6f58c1b31eb57f82a8e1c325fa
YadhuKrishnanK <yadhunandan97@gmail.com>
2025-09-12 16:00:44 +0200

    deleted anchor building blocks

---
+++ Assets/Scripts/AnchorPlacement.cs
@@ -0,0 +1,20 @@
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
+using System;
+using UnityEngine;
+
+public class AnchorPlacement : MonoBehaviour
+{
+    public GameObject anchorPrefab;
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput. Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            CreateSpatialAnchor();
+        }
+    }
+
+    public void CreateSpatialAnchor()
+    {
+        GameObject prefab = Instantiate(anchorPrefab, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch), OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch));
+    }
+}
--- Assets/Scripts/PreviewMarker.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine;
-
-// Put this on the preview object INSIDE your anchored prefab
-public class PreviewMarker : MonoBehaviour {}
